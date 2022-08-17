77bf4d832df33461b6af57b2eabbdf72795ebe78
Haki <defvermillion@gmail.com>
2022-08-17 12:42:43 +0300

    continue

--- script.js
+++ script.js
@@ -34,62 +34,95 @@
 //   } 
 // }
 
-let result = '';
-const lenght = 8;
+// let result = '';
+// const lenght = 8;
 
-for (let i = 1; i < lenght; i++) {
-  for (let j = 0; j < i; j++) {
-      result += "*";
-  }
+// for (let i = 1; i < lenght; i++) {
+//   for (let j = 0; j < i; j++) {
+//       result += "*";
+//   }
 
-  result += '\n';
+//   result += '\n';
+// }
+// console.log(result);
+
+
+// // First task
+// let num = 5;
+
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+// // Second task
+// for (let i = 20; i < 21; i--) {
+//   if (i === 13) {
+//     break;
+//   }
+
+
+//   console.log(i);
+// }
+// // Third task
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+// // Fourth task
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//       continue;
+//   } else {
+//       console.log(i);
+//   }
+// }
+
+//  num = 2;
+//  while (num <= 16 ) {
+//    if (num % 2 === 0 ) {
+//     num++;
+//     continue;
+//   } else {
+//     console.log(num);
+//     num++;
+//   }
+//  }
+
+ //Fifth task
+//  const arrayOfNumbers = [];
+
+//     for (let i = 5; i < 11; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+
+//     const storeName = 'Films';
+
+//     const storeDescription = {
+//     budget: 10000,
+//     employees: ['Ivan', 'Fedya', 'Bogdan'],
+//     products: {
+//         moloko: 1000,
+//         hleb: 500
+//     },
+// };
+
+
+// const a = ["a", "b", "c"];
+// for (let i = 0; i < a.length; ++i) {
+//     console.log(a[i]);
+// }
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.lenght; i++) {
+    result[i] = arr[i];
 }
+
 console.log(result);
+return result;
 
-
-// First task
-let num = 5;
-
-while (num <= 10) {
-  console.log(num);
-    num++;
-    if (num === -6) {
-      break;
-    }
-}
-// Second task
-for (let i = 20; i < 21; i--) {
-  if (i === 13) {
-    break;
-  }
-
-
-  console.log(i);
-}
-// Third task
-for (let i = 2; i <= 10; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
-// Fourth task
-for (let i = 2; i <= 16; i++) {
-  if (i % 2 === 0) {
-      continue;
-  } else {
-      console.log(i);
-  }
-}
-
- num = 2;
- while (num <= 16 ) {
-   num++;
-   if (num % 2 === 0 ) {
-    continue;
-  } else {
-    console.log(num);
-  }
-  if (num === 3) {
-    num++;
-  }
- }
