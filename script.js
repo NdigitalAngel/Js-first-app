6f96a224993365e54cf7b25cb09d3facf75fe74d
Haki <defvermillion@gmail.com>
2022-08-19 19:32:12 +0300

    kino app

--- script.js
+++ script.js
@@ -1,128 +1,36 @@
 "use strict";
 
 
-// const numberOfFilms = +prompt( 'Сколько фильмов вы уже смотрели?', '');
+const numberOfFilms = +prompt( 'Сколько фильмов вы уже смотрели?', '');
 
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privat: false
-//  };
-//  let x = 'Cool';
-// const  a = prompt('Один из просмотренных фильмов?', ''),
-//        b = prompt('На сколько оцените его?', ''),
-//        c = prompt('Один из просмотренных фильмов?', ''),
-//        d = prompt('На сколько оцените его?', '');
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+ };
 
-// console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+  const  a = prompt('Один из просмотренных фильмов?', ''),
+         b = prompt('На сколько оцените его?', '');
 
-
-
-// while(num < 55) {
-//   console.log(num);
-//   num++;
-// }
-
-// for (let i = 0; i < 3; i++) {
-//   console.log(i);
-//   for (let j = 0; j < 3; j++) {
-//     console.log(j);   
-//   } 
-// }
-
-// let result = '';
-// const lenght = 8;
-
-// for (let i = 1; i < lenght; i++) {
-//   for (let j = 0; j < i; j++) {
-//       result += "*";
-//   }
-
-//   result += '\n';
-// }
-// console.log(result);
-
-
-// // First task
-// let num = 5;
-
-// while (num <= 10) {
-//   console.log(num);
-//   num++;
-// }
-// // Second task
-// for (let i = 20; i < 21; i--) {
-//   if (i === 13) {
-//     break;
-//   }
-
-
-//   console.log(i);
-// }
-// // Third task
-// for (let i = 2; i <= 10; i++) {
-//   if (i % 2 === 0) {
-//     console.log(i);
-//   }
-// }
-// // Fourth task
-// for (let i = 2; i <= 16; i++) {
-//   if (i % 2 === 0) {
-//       continue;
-//   } else {
-//       console.log(i);
-//   }
-// }
-
-//  num = 2;
-//  while (num <= 16 ) {
-//    if (num % 2 === 0 ) {
-//     num++;
-//     continue;
-//   } else {
-//     console.log(num);
-//     num++;
-//   }
-//  }
-
- //Fifth task
-//  const arrayOfNumbers = [];
-
-//     for (let i = 5; i < 11; i++) {
-//         arrayOfNumbers[i - 5] = i;
-//     }
-
-//     console.log(arrayOfNumbers);
-//     return arrayOfNumbers;
-
-//     const storeName = 'Films';
-
-//     const storeDescription = {
-//     budget: 10000,
-//     employees: ['Ivan', 'Fedya', 'Bogdan'],
-//     products: {
-//         moloko: 1000,
-//         hleb: 500
-//     },
-// };
-
-
-// const a = ["a", "b", "c"];
-// for (let i = 0; i < a.length; ++i) {
-//     console.log(a[i]);
-// }
-
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
-
-for (let i = 0; i < arr.lenght; i++) {
-    result[i] = arr[i];
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done'); 
+      } else {
+        console.log('error');
+        i--;
+      }
+}
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('ERROR BlyAD'); 
 }
 
-console.log(result);
-return result;
-
+console.log(personalMovieDB);
