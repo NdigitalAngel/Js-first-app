38890e0cb8024830c565a29f3fb534bb99c146da
Haki <defvermillion@gmail.com>
2022-08-27 14:04:50 +0300

    add new functions upgrading code

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 "use strict";
 
 
-const numberOfFilms = +prompt( 'Сколько фильмов вы уже смотрели?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt( 'Сколько фильмов вы уже смотрели?', '');
+
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt( 'Сколько фильмов вы уже смотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -11,42 +21,53 @@ const personalMovieDB = {
   privat: false
  };
 
-for (let i = 0; i < 2; i++) {
-  const  a = prompt('Один из просмотренных фильмов?', ''),
-         b = prompt('На сколько оцените его?', '');
-
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done'); 
-      } else {
-        console.log('error');
-        i--;
-      }
-}
-if (personalMovieDB.count < 10) {
-  console.log('Просмотрено мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-  console.log('Вы киноман');
-} else {
-  console.log('ERROR BlyAD'); 
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const  a = prompt('Один из просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
+  
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) { 
+          personalMovieDB.movies[a] = b;
+          console.log('done'); 
+        } else {
+          console.log('error');
+          i--;
+        }
+  }
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
 
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('ERROR BlyAD'); 
+  }
+} 
 
+detectPersonalLevel();
 
-function createCounter() {
-  let counter = 0;
-  const myFunction = function() {
-    counter = counter + 1;
-    return counter;
-  };
-   return myFunction;
+function showMyDB() {
+  if (personalMovieDB.privat === false) {
+    console.log(personalMovieDB);
+  }
 }
-const increment = createCounter();
-const c1 = increment();
-const c2 = increment();
-const c3 = increment();
-console.log('example increment', c1, c2, c3);
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}.`, '');
+  }
+}
+
+writeYourGenres();
+
+
+
+
